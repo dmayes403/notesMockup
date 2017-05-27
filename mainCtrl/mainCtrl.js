85fc8e64c3f130229ccc8a5df8f42b7a977d7cce
dmayes403 <davidrmayes@gmail.com>
2017-05-26 21:14:50 -0600

    all data is accessible. Next step is to be able to add a note

--- mainCtrl/mainCtrl.js
+++ mainCtrl/mainCtrl.js
@@ -2,4 +2,14 @@ angular.module('notesApp')
   .controller('mainCtrl', function($scope, mainSrvc){
       $scope.notes = mainSrvc.getNotes();
       console.log($scope.notes);
+
+      $scope.setCurrentNoteTopics = function(NoteTopics){
+        $scope.CurrentNoteTopics = NoteTopics.notes;
+        console.log($scope.CurrentNoteTopics);
+      }
+
+      $scope.setCurrentNote = function(NoteObject){
+        $scope.currentNote = NoteObject.note;
+        console.log($scope.currentNote);
+      }
   })
